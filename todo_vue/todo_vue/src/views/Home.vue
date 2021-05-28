2aea6f3248ba5c73ab28d365d341b12e79da7235
Simone Westphal <hello@swestphal.io>
2021-05-28 17:25:24 +0200

    refactor: Add todo/done functionality

--- todo_vue/todo_vue/src/views/Home.vue
+++ todo_vue/todo_vue/src/views/Home.vue
@@ -7,13 +7,13 @@
 
     <div class="columns">
       <div class="column is-3 is-offset-3">
-        <form >
+        <form @submit.prevent="addTask">
           <h2 class="subtitle">Add task</h2>
 
           <div class="field">
             <label class="label">Description</label>
             <div class="control">
-              <input class="input" type="text">
+              <input class="input" type="text" v-model="description">
             </div>
           </div>
 
@@ -21,7 +21,7 @@
             <label class="label">Status</label>
             <div class="control">
               <div class="select">
-                <select>
+                <select v-model="status">
                   <option value="todo">To do</option>
                   <option value="done">Done</option>
                 </select>
@@ -43,11 +43,11 @@
         <h2 class="subtitle">To do</h2>
 
         <div class="todo">
-          <div class="card" v-for >
-            <div class="card-content">description</div>
+          <div class="card" v-for="todo in todos"  :key="todo.id" >
+            <div class="card-content">{{ todo.description }}</div>
 
             <footer class="card-footer">
-              <a class="card-footer-item" >Done</a>
+              <a class="card-footer-item" @click="setStatus(todo.id,'done')">Done</a>
             </footer>
           </div>
         </div>
@@ -56,11 +56,11 @@
         <h2 class="subtitle">Done</h2>
 
         <div class="done">
-          <div class="card" >
-            <div class="card-content">description</div>
+          <div class="card" v-for="done in dones" :key="done.id">
+            <div class="card-content">{{ done.description }}</div>
 
             <footer class="card-footer">
-              <a class="card-footer-item" >To do</a>
+              <a class="card-footer-item" @click="setStatus(done.id,'todo')">To do</a>
             </footer>
           </div>
         </div>
@@ -71,24 +71,75 @@
 <script >
 // @ is an alias to /src
 import axios from 'axios';
-import {onMounted, ref} from "vue";
+import {onMounted, ref, computed} from "vue";
 
 export default {
   name: 'Home',
   setup() {
     const tasks = ref([])
+    const description=ref("")
+    const status=ref("todo")
 
     const load = async()=> {
-
-      const response = await axios.get('tasks')
+      const response = await axios.get('tasks/')
       tasks.value = response.data
+    }
+
+    const setStatus = (id,status) => {
+      const task = tasks.value.filter(task => task.id === id)[0]
+
+      axios.put('tasks/'+id+'/', {
+        status:status,
+        description:task.description
+      }).then(()=> {
+        task.status = status
+      })
 
     }
 
+    const addTask = () => {
+      if (description.value) {
+        axios.post('tasks/', {
+            description: description.value,
+            status: status.value
+          }).then( (response) => {
+            // build new task
+            const newTask = {
+              'id':response.data.id,
+              'description':response.data.description,
+              'status':response.data.status
+            }
+            // push new task in tasks-array
+            tasks.value.push(newTask)
+
+            // set back and clean input fields
+            description.value=""
+            status.value="todo"
+          }).catch((error) => {
+            console.log(error)
+          })
+        }
+      }
+
+
     onMounted(load)
+
+    const todos = computed (()=> {
+        return tasks.value.filter(task => task.status==='todo')
+      })
+    const dones = computed (()=> {
+        return tasks.value.filter(task => task.status==='done')
+      })
+
     return {
       tasks,
-      load
+      load,
+      addTask,
+      setStatus,
+      todos,
+      dones,
+      description,
+      status
     }
   }
 }
