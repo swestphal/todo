97ef67696abd4e46fcc5a61270d6ad60596de321
Simone Westphal <hello@swestphal.io>
2021-05-28 17:26:19 +0200

    refactor: Django backend

--- todo_django/task/views.py
+++ todo_django/task/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Task
+from .serializers import TaskSerializer
+
+from rest_framework import viewsets
+from rest_framework.authentication import  BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class TaskViewSet(viewsets.ModelViewSet):
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
--- todo_django/todo_django/urls.py
+++ todo_django/todo_django/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from task.views import TaskViewSet
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
